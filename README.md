f19c437871604c8860a3ead3c50c2a7af82e5100
Manuel Valle <manuvaldi@gmail.com>
2019-01-16 15:35:03 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Openstack Floating IP updates handler
+
+Python script to monitor and handler Floating IP events from Openstack neutron. This script create a queue with `auto_delete` feature binding to `neutron` exchange with `routing_key` equal to `notifications.info`
+
+## Installation
+```
+pip install -r requirements.txt
+```
+
+## Use
+```
+python fliatingip_handler.py -c floatingip_handler.config.example
+```
+
+## Configuration
+```
+[Default]
+debug = False
+
+[RabbitMQ]
+monitoring_queue_name = floatingip
+rabbitmq_hosts = rabbitmq01,rabbitmq01,rabbitmq01
+rabbitmq_port = 5672
+user = guest
+pass = guest
+```
+Options:
+- `debug`: Enable or disable debug trace logs.
+- `monitoring_queue_name`: RabbitMQ queue name to create and manage (auto_delete queue)
+- `rabbitmq_hosts`: Comman-separated list of rabbitmq servers
+- `rabbitmq_port`: RabbitMQ port
+- `user`: RabbitMQ user name
+- `pass`: RabbitMQ password
+
+## Configure/Dev handler actions
+By default, no action is configure to do either UP or DOWN floating IP. If you want to handler these actions you should modified the code. There are 2 functions on the top of script:
+
+- `callback_create_floating_ip`: Handler function for floating IP association
+- `callback_delete_floating_ip`: Handler function for floating IP disassociation
+
+An example:
+
+```
+from httplib2 import Http
+def callback_delete_floating_ip(data):
+    """Callback function for floating ip disassociation"""
+    h = Http()
+    print " * Floating IP %s disassociated" % data['floating_ip_address']
+    unregister = json.loads(urllib2.urlopen("http://10.10.10.10/release/ipv4address?ip_address="+data['floating_ip_address']).read())
+
+
+
+
+
+```
---
+++ floatingip_handler.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+"""Openstack Floating IP updates handler script"""
+
+import argparse
+import ast
+import ConfigParser
+import json
+import os.path
+import pika
+
+
+# Callback functions (change as needed)
+def callback_delete_floating_ip(data):
+    """Callback function for floating ip disassociation"""
+    print " * Floating IP %s disassociated" % data['floating_ip_address']
+
+
+def callback_create_floating_ip(data):
+    """Callback function for floating ip association"""
+    print " * Floating IP {0} associated to {1}".format(
+        data['floating_ip_address'], data['fixed_ip_address'])
+
+
+# Arguments
+parser = argparse.ArgumentParser(
+    description='Monitoring and handler neutron floating IP updates')
+parser.add_argument('-c', '--configfile',
+                    default="./floatingip_handler.config",
+                    help='file to read the config from')
+args = vars(parser.parse_args())
+
+# Check if config file exists
+if not os.path.isfile(args['configfile']):
+    print " * ERROR: config file doesn't exist"
+    exit(1)
+
+# Read config
+config_parser = ConfigParser.RawConfigParser()
+config_parser.read(args['configfile'])
+
+DEBUG = ast.literal_eval(config_parser.get('Default', 'debug'))
+if DEBUG:
+    print " [*] Debug mode enabled"
+
+MONITORING_QUEUE = config_parser.get('RabbitMQ', 'monitoring_queue_name')
+RABBITMQ_HOSTS = config_parser.get('RabbitMQ', 'rabbitmq_hosts').split(',')
+RABBITMQ_PORT = int(config_parser.get('RabbitMQ', 'rabbitmq_port'))
+RABBITMQ_USER = config_parser.get('RabbitMQ', 'user')
+RABBITMQ_PASS = config_parser.get('RabbitMQ', 'pass')
+
+
+# Functions
+def _process_msg(_channel, _method, _properties, body):
+    # pylint: disable=W0612,W0613
+
+    payload = json.loads(body)
+
+    if 'event_type' in payload:
+        event_type = payload['event_type']
+        payload_data = payload['payload']
+
+        if DEBUG:
+            print " Received: %r" % event_type
+        if DEBUG:
+            print json.dumps(payload, sort_keys=True,
+                             indent=4, separators=(',', ': '))
+
+        if event_type == 'floatingip.update.start':
+            print " * Starting updating floating IP"
+
+        elif event_type == 'floatingip.update.end':
+
+            if payload_data['floatingip']['status'] == 'ACTIVE':
+                callback_create_floating_ip(payload_data['floatingip'])
+
+            elif payload_data['floatingip']['status'] == 'DOWN':
+                callback_delete_floating_ip(payload_data['floatingip'])
+
+            else:
+                print " * Floating IP unknown operation"
+                if DEBUG:
+                    print json.dumps(payload, sort_keys=True, indent=4,
+                                     separators=(',', ': '))
+    else:
+        if DEBUG:
+            print " * Other event"
+            print json.dumps(payload, sort_keys=True, indent=4,
+                             separators=(',', ': '))
+
+
+# Main loop
+while True:
+    for mq_host in RABBITMQ_HOSTS:
+        try:
+            print " [*] Connecting %s ..." % mq_host
+            credentials = pika.PlainCredentials(RABBITMQ_USER,
+                                                RABBITMQ_PASS)
+            parameters = pika.ConnectionParameters(mq_host, RABBITMQ_PORT,
+                                                   '/', credentials)
+            connection = pika.BlockingConnection(parameters)
+            channel = connection.channel()
+
+            channel.queue_declare(queue=MONITORING_QUEUE,
+                                  auto_delete=True)
+            channel.queue_bind(queue=MONITORING_QUEUE,
+                               exchange='neutron',
+                               routing_key='notifications.info')
+
+            channel.basic_consume(_process_msg,
+                                  queue=MONITORING_QUEUE,
+                                  no_ack=True)
+
+            print ' [*] Waiting for floating IP changes. To exit press CTRL+C'
+            channel.start_consuming()
+
+        # Do not recover on channel errors
+        except pika.exceptions.AMQPChannelError:
+            break
+        # Recover on all other connection errors
+        except pika.exceptions.AMQPConnectionError:
+            print " * ERROR Connection"
+            continue
